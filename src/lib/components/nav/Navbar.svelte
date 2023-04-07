156108b7b8832d73c90c94b6f8d55cb0b7033400
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2023-04-07 22:32:29 +0530

    nav twick

--- src/lib/components/nav/Navbar.svelte
+++ src/lib/components/nav/Navbar.svelte
@@ -10,14 +10,14 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between h-16">
         <div class="flex items-center">
-          <a class="flex-shrink-0 text-white" href="#">
+          <a class="flex-shrink-0 text-white" href="/">
             <Logo/>
           </a>
           <div class="hidden md:block">
             <div class="ml-10 flex items-baseline space-x-4">
               <a href="/" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
   
-              <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+              <a href="/" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
   
               <a href="/contact" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
             </div>
@@ -25,7 +25,7 @@
         </div>
         <div class="-mr-2 flex md:hidden">
           <!-- Mobile menu button -->
-          <button on:click={handleClick} type="button" class=" bg-pink-500 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <button on:click={handleClick} type="button" class=" bg-pink-500 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white  hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-pink-600 focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <!-- Icon when menu is closed. -->
             <!--
@@ -58,11 +58,11 @@
     {#if showNev}
     <div class="block md:hidden" id="mobile-menu">
       <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-        <a href="#" class="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
+        <a href="/" class="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
   
-        <a href="#" class="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+        <a href="/" class="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
   
-        <a href="#" class="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
+        <a href="/contact" class="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
     </div>
 </div> 
 {/if}
