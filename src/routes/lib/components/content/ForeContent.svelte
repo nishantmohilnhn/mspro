42aae923d930bfc208b1fd177de45ee610196c42
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2023-04-07 19:22:20 +0530

    pro added

---
+++ src/routes/lib/components/content/ForeContent.svelte
@@ -0,0 +1,44 @@
+<section class="bg-white py-16">
+    <div class="container mx-auto">
+      <h2 class="text-3xl font-bold mb-8">Product Design</h2>
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-1/2 lg:w-1/3 mb-8">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <h3 class="text-xl font-semibold mb-4">Concept Development</h3>
+            <p class="text-gray-600">Our team works with you to develop a concept that meets the needs of your target market.</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 mb-8">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <h3 class="text-xl font-semibold mb-4">Prototyping</h3>
+            <p class="text-gray-600">We create prototypes to test the functionality and design of your product before production.</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 mb-8">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <h3 class="text-xl font-semibold mb-4">Production</h3>
+            <p class="text-gray-600">Our team works with you to ensure a successful production process, from start to finish.</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 mb-8">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <h3 class="text-xl font-semibold mb-4">Packaging Design</h3>
+            <p class="text-gray-600">We can help you design attractive packaging for your products that stands out on the shelf.</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 mb-8">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <h3 class="text-xl font-semibold mb-4">Product Testing</h3>
+            <p class="text-gray-600">We conduct product testing to ensure that your product meets all necessary standards and regulations.</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 lg:w-1/3 mb-8">
+          <div class="bg-gray-100 p-6 rounded-lg">
+            <h3 class="text-xl font-semibold mb-4">Marketing Support</h3>
+            <p class="text-gray-600">We can help you develop marketing strategies to effectively promote your product.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
